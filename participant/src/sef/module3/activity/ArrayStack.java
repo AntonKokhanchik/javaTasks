aa2f62742f80049fd779a8596c24c79efd165893
anton.kokhanchik <anton.kokhanchik@accenture.com>
2019-03-26 12:26:39 +0300

    module3 activity done

--- participant/src/sef/module3/activity/ArrayStack.java
+++ participant/src/sef/module3/activity/ArrayStack.java
@@ -2,8 +2,8 @@ package sef.module3.activity;
 
 
 /**
- * @author John Doe
- * 
+ * @author Anton Kokhanchik
+ *  *
  * This class represents a stack structure (Last-In, First-Out) implemented using
  * an array
  */
@@ -27,21 +27,22 @@ public class ArrayStack{
 		//TODO #1: Write a decision making code such that if the value
 		//of the integer parameter variable 'maximum' is less than 0 
 		//If true, then then the variable 'maximum' should be set to 1
-		
+		if (maximum < 0)
+			maximum = 1;
 		//END TODO #1
 		
 		
 		
 		//TODO #2: Write code that will initialize the array 'stack' as an 
 		//array of objects with a maximum length specified by the parameter 'maximum'
-		
+		stack = new Object[maximum];
 		//END TODO #2
 	
 		
 		
 		//TODO #3: Write code that will initialize the value of the 
 		//integer variable 'count' to 0
-	
+		count = 0;
 		//END TODO #3
 	}
 	
@@ -56,13 +57,15 @@ public class ArrayStack{
 		
 		//TODO #4: Write decision making code to see if the value of 'count' is 0.  
 		//If true, set the value of 'temp' to null
-		
+		if (count == 0)
+			//temp = null; //this is not right
+			return null;
 		//END TODO #4
 		
 		
 		//TODO #5: Write code that will assign to temp, the 
 		//last element pushed into this array
-		
+		temp = stack[count-1];
 		//END TODO #5
 		
 		return temp;
@@ -79,7 +82,8 @@ public class ArrayStack{
 		//TODO #6: Write decision making code that checks if there is enough 
 		//space in the array to store one more 'element'.  If there is no more space, 
 		//return false
-		
+		if (count == stack.length)
+			return false;
 		//END TODO #6
 		
 		
@@ -87,7 +91,7 @@ public class ArrayStack{
 		//TODO #7: Write code that will add the Object parameter 'element' to the next
 		//available index of the array. The integer variable 'count' should be increased
 		//by 1 to indicate this addition to the array
-		
+		stack[count++] = element;
 		//END TODO #7
 		
 		return true;
@@ -104,19 +108,20 @@ public class ArrayStack{
 		
 		//TODO #8: Write code that will check if there are any elements contained in 
 		//the 'stack' array.  If there array is empty, return null
-		
+		if (count == 0)
+			return null;
 		//END TODO #8
 		
 		
 		//TODO #9: Write code that will subtract 1 from the variable 'count' to represent an
 		//element being 'removed' from the array
-		
+		count--;
 		//END TODO 9
 		
 		
 		//TODO #10: Write code that will get the last element added in the array and assign it
 		//to the variable 'temp'
-	
+		temp = stack[count];
 		//END TODO #10
 		
 	
@@ -137,7 +142,11 @@ public class ArrayStack{
 		//is specified in the parameter and return its position relative to the end
 		//of the array. 
 		//Ex: An element found at the top of the stack will be at position 0
-		
+		int pos = 0;
+		int l = stack.length-1;
+		for (;l-pos >= 0 ; pos++)
+			if (stack[l-pos] == element)
+				return pos;
 	
 		//END TODO #11:
 		
